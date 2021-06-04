6ee8a35d6eec4c55f02a9cce18c5017f41026f1d
ruanvalente <ruanopensuselinux@gmail.com>
2021-06-04 14:44:39 -0300

    chore: added axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { useEffect } from "react";
 
-import { dtMoneyService } from "./services/server";
-
 import { Header } from "./components/Header";
 import { DashBoard } from "./components/Dashboard";
 
 import { GlobalStyle } from "./styles/globalStyle";
 
+import { dtMoneyService } from "./services/server";
+import { api } from "./services/api";
+
 dtMoneyService();
 
 export function App() {
   useEffect(() => {
-    fetch("htpp://localhost:3000/api/v1/transaction")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    api.get("transaction").then((response) => console.log(response.data));
   }, []);
 
   return (
